9c7b1a36eee5ca0fccef44c797de15a6fad09ced
Emre Cenk <emrecenk9@gmail.com>
2021-12-10 18:08:34 -0500

    added authentication

--- main.py
+++ main.py
@@ -1,16 +1,41 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from os import environ
+import requests
+from requests.auth import HTTPBasicAuth
+
+class github_stat_generator():
 
 
-# Press the green button in the gutter to run the script.
+    def __init__(self, github_username: str = "",
+                 github_password: str = ""):
+        """
+        :param github_username: if you want to authenticate, this will be your username
+        :param github_password: if you want to authenticate, this will be your password
+        Note: github only lets 60 requests per hour if you're not authenticated.
+        """
+        self.authentication = HTTPBasicAuth(github_username, github_password)
+
+
+    def login(self, github_username: str, github_password: str):
+        """
+        :param github_username: username to log in
+        :param github_password: password to log in
+        return: None
+
+        If you run out of requests, use this function to authenticate requests.
+        If this function is ran once, every request after that will be authenticated with these credentials.
+        Note: github only lets 60 requests per hour if you're not authenticated.
+        """
+        self.authentication = HTTPBasicAuth(github_username, github_password)
+
+
+    # def login(self, username: str, password: str):
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    from dotenv import load_dotenv
+    load_dotenv()
+    username = environ["GITHUB_USERNAME"]
+    password = environ["GITHUB_PASSWORD"]
+    self = github_stat_generator(username, password)
